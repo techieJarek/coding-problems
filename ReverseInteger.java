41269da291c7598a700e1cf40a1b543bbac92ad7
techieJarek <47065719+techieJarek@users.noreply.github.com>
2019-08-18 20:23:45 +0100

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−2^31,  2^31 − 1].
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ */
+public class ReverseInteger {
+    public static void main(String[] args){
+        System.out.println("Reversed int : " + reverse(-123));
+    }
+
+    private static int reverse(int i) {
+        if(i==0){
+            return 0;
+        }
+        boolean isPositive = true;
+        if(i < 0){
+            isPositive = false;
+            i = i * (-1);
+        }
+        int reversed = 0;
+        List<Integer> digits = getDigits(i);
+        int decimalIterator = 1;
+
+        for(int digit: digits){
+            try {
+                int multipliedDigit = Math.multiplyExact(digit, decimalIterator);
+                reversed = Math.addExact(reversed, multipliedDigit);
+            } catch (ArithmeticException e) {
+                return 0;
+            }
+            decimalIterator = decimalIterator * 10;
+        }
+
+        if(reversed < 2147483647 + 1 || reversed > 2147483647){
+            return 0;
+        }
+
+        return (isPositive) ? reversed : reversed * (-1);
+    }
+
+    private static List<Integer> getDigits(int i) {
+        List<Integer> list = new ArrayList<>();
+
+        while (i >= 1) {
+            list.add(0, i % 10);
+            i = i/10;
+        }
+
+        return list;
+    }
+}
