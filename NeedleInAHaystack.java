fd68b0b3759104a18fc0e5ebbebe269f7f85b051
techieJarek <47065719+techieJarek@users.noreply.github.com>
2020-01-05 20:01:31 +0000

    Create NeedleInAHaystack.java

---
+++ NeedleInAHaystack.java
@@ -0,0 +1,30 @@
+/**
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Example 1:
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * Example 2:
+ *
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ */
+public class NeedleInAHaystack {
+    public static void main (String[] args){
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println("First index of needle: " + getIndex(haystack, needle));
+    }
+
+    private static int getIndex(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+}
