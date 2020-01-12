97c7ee8766e608959e8f86e4c62a66d24d078f26
techieJarek <47065719+techieJarek@users.noreply.github.com>
2020-01-12 18:50:53 +0000

    Update AddTwoNumbersAsLinkedLists.java

--- AddTwoNumbersAsLinkedLists.java
+++ AddTwoNumbersAsLinkedLists.java
@@ -22,6 +22,54 @@ NOTE: BigInteger slows down performance but was used here to pass the test on ve
 public class AddTwoNumbersAsLinkedLists {
     public static void main(String args[]){
         addTwoNumbers(getListNode(new BigInteger("342")), getListNode(new BigInteger("465")));
+        
+        // Alternative, cleaner approach
+        ListNode n1 = new ListNode(2);
+        ListNode n11 = new ListNode(4);
+        ListNode n111 = new ListNode(3);
+        n1.next = n11;
+        n11.next = n111;
+        ListNode n2 = new ListNode(5);
+        ListNode n22 = new ListNode(6);
+        ListNode n222 = new ListNode(4);
+        n2.next = n22;
+        n22.next = n222;
+
+        printListValues(n1);
+        printListValues(n2);
+        printListValues(addTwoNumbersAlternativeApproach(n1, n2));
+
+    }
+    
+    private static void printListValues(ListNode node) {
+        while(node!=null){
+            System.out.print(node.val);
+            if(node.next!=null){
+                System.out.print("->");
+            }
+            node = node.next;
+        }
+        System.out.println("");
+    }
+
+    public static ListNode addTwoNumbersAlternativeApproach(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode first = l1, second = l2, current = dummyHead;
+        int carry = 0;
+        while (first!=null || second != null){
+            int x = (first!=null) ? first.val : 0;
+            int y = (second!=null) ? second.val : 0;
+            int sum = x + y + carry;
+            carry = sum/10;
+            current.next = new ListNode(sum % 10);
+            if (first!=null) first = first.next;
+            if (second!=null) second = second.next;
+            current = current.next;
+        }
+        if(carry > 0){
+            current.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
     public static class ListNode {
