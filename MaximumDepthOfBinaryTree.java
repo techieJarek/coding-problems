9aab080243c9f95d9c94190b8e4ad6800a5aa52f
techieJarek <47065719+techieJarek@users.noreply.github.com>
2020-01-13 19:33:26 +0000

    Update MaximumDepthOfBinaryTree.java

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -1,3 +1,8 @@
+/**
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node
+ * down to the farthest leaf node.
+ */
 public class MaximumDepthOfBinaryTree {
     public static void main(String[] args){
         System.out.println("Maximum depth of the tree is : " + getDepth(getTree()));
