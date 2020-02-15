a197bfad18bb60769a0a6e89bd6e1a41538f5e10
Jerry <47065719+techieJarek@users.noreply.github.com>
2020-02-15 12:21:26 +0000

    Update FibonacciNumbers.java

--- FibonacciNumbers.java
+++ FibonacciNumbers.java
@@ -1,22 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.time.Instant;
 /**
- * Write a function that computes the list of the first 100 Fibonacci numbers.
+ * Write a function that computes Fibonacci sequence.
  * By definition, the first two numbers in the Fibonacci sequence are 0 and 1,
  * and each subsequent number is the sum of the previous two.
  * As an example, here are the first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
  */
-public class FibonacciNumbers {
+public class FibonacciNumber {
+
     public static void main(String[] args){
-        List<Double> sequence = new ArrayList<>();
-        sequence.add(Double.valueOf(0));
-        sequence.add(Double.valueOf(1));
-        System.out.print("0, 1, ");
-        for(int i=2; i<=100; i++){
-            Double newNumber = sequence.get(i-2) + sequence.get(i-1);
-            System.out.print(String.format("%.0f", newNumber) + ", ");
-            sequence.add(newNumber);
-        }
+        int n = 40;
+        Instant before = Instant.now();
+        System.out.println("Fibonacci Number for n "+ n + " equals : " + fib(n));
+        Instant after = Instant.now();
+        System.out.println(after.toEpochMilli() - before.toEpochMilli());
+
+        before = Instant.now();
+        System.out.println("Fibonacci Number for n "+ n + " with memo equals : " + fibWithMemo(n));
+        after = Instant.now();
+        System.out.println(after.toEpochMilli() - before.toEpochMilli());
+
+    }
+
+    private static int fib(int N) {
+        if(N<=1) return N;
+        return fib(N-1)+fib(N-2);
+    }
+
+    private static int fibWithMemo(int N) {
+        return getFibWithMemo(N,new int[N+1]);
+    }
+
+    private static int getFibWithMemo(int N, int[] memo){
+        if(N<=1) return N;
+        if(memo[N]!=0) return memo[N];
+        memo[N]=fib(N-1)+fib(N-2);
+        return memo[N];
     }
 }
+
